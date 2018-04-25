75b0a74701fd12de64e3bb8bfb41d5ab8106ecb5
Eric <ehannum@gmail.com>
2018-04-25 00:09:06 -0700

    minimum basic working product

--- server/server.js
+++ server/server.js
@@ -1,19 +1,50 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var querystring = require('querystring');
 var http = require('http');
 var server = http.Server(app);
-var path = require('path');
-
+var ffmpeg = require('fluent-ffmpeg');
+var fileupload = require('express-fileupload');
 var fs = require('fs');
 
 // -- SERVE STATIC FILES and JSON
 
 app.use(express.static('public'));
+app.use(fileupload());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// -- FFMPEG
+
+app.post('/jam', function (req, res) {
+
+  var gif = req.files.gif;
+  var mp3 = req.files.mp3;
+
+  fs.writeFileSync('./server/tmp/in.gif', gif.data);
+  fs.writeFileSync('./server/tmp/in.mp3', mp3.data);
+
+  // ffmpeg -ignore_loop 0 -i animation.gif -i audio.mp3 -shortest -c:v libvpx -threads 4 -b:v 400k -f webm out.webm
+
+  var webmWriteStream = fs.createWriteStream('./public/out.webm');
+
+  var vid = ffmpeg()
+    .input('./server/tmp/in.gif')
+    .inputOptions('-ignore_loop 0')
+    .input('./server/tmp/in.mp3')
+    .outputOptions('-shortest')
+    .outputFormat('webm')
+    .videoCodec('libvpx')
+    .on('start', function (command) {
+      console.log('Starting ' + command);
+    })
+    .on('end', function () {
+      console.log('Finished Processing');
+      res.send('<video><source src="out.webm" type="video/webm" controls></video>');
+    })
+    .on('error', function (err) {
+      console.log(err.message);
+      res.send(err.message);
+    })
+    .pipe(webmWriteStream, {end: true});
+});
 
 // -- START SERVER
 
